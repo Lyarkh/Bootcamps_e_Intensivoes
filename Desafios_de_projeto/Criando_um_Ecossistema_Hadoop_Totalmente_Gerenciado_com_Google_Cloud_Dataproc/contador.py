b9848f6c77926b4eba43fbcd4e56d68f6f0ea285
Lucas Emanuel <82418777+Lyarkh@users.noreply.github.com>
2022-03-31 11:09:15 -0300

    Corrigindo bugs

--- Desafios_de_projeto/Criando_um_Ecossistema_Hadoop_Totalmente_Gerenciado_com_Google_Cloud_Dataproc/contador.py
+++ Desafios_de_projeto/Criando_um_Ecossistema_Hadoop_Totalmente_Gerenciado_com_Google_Cloud_Dataproc/contador.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     sc = SparkContext("local","PySpark - Desafio Dataproc")
     words = sc.textFile("gs://desafio-dataproc/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    wordCounts.saveAsTextFile("gs://desafio-dataproc/resultado.txt")
+    wordCounts.saveAsTextFile("gs://desafio-dataproc/resultado")
