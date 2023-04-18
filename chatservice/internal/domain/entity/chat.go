c4d371f3e9e0001065a5b5e403d0be6bac078a0f
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-04-18 08:44:58 -0300

    feat: adicionando novos atributos da struct ChatConfig

--- chatservice/internal/domain/entity/chat.go
+++ chatservice/internal/domain/entity/chat.go
@@ -1,7 +1,14 @@
 package entity
 
 type ChatConfig struct {
-	Model *Model
+	Model            *Model
+	Temperature      float32  // 0.0 to 1.0
+	TopP             float32  // 0.0 to 1.0 - to a low value, like 0.1, the model will be very conservative in its word choices, and will tend to generate relatively predictable prompts
+	N                int      // number of messages to generate
+	Stop             []string // list of tokens to stop on
+	MaxTokens        int      // number of tokens to generate
+	PresencePenalty  float32  // -2.0 to 2.0 - Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+	FrequencyPenalty float32  // -2.0 to 2.0 - Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, increasing the model's likelihood to talk about new topics.
 }
 
 type Chat struct {
