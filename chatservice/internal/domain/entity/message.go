3b85484e2840f60e824d4747d03c5a76927ee6c9
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-04-18 08:53:19 -0300

    feat: adicionando construtor do struct e validação na inicialização

--- chatservice/internal/domain/entity/message.go
+++ chatservice/internal/domain/entity/message.go
@@ -1,3 +1,46 @@
 package entity
 
-type Message struct {}
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Message struct {
+	ID        string
+	Role      string
+	Content   string
+	Tokens    int
+	Model     *Model
+	CreatedAt time.Time
+}
+
+func NewMessage(role string, content string, model *Model) (*Message, error) {
+	msg := &Message{
+		ID:        uuid.New().String(),
+		Role:      role,
+		Content:   content,
+		Model:     model,
+		CreatedAt: time.Now(),
+	}
+	if err := msg.Validate(); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
+
+func (m *Message) Validate() error {
+	if m.Role != "user" && m.Role != "system" && m.Role != "assistant" {
+		return errors.New("Invalid role")
+	}
+	if m.Content == "" {
+		return errors.New("content is empty")
+	}
+	if m.CreatedAt.IsZero() {
+		return errors.New("invalid created at")
+	}
+
+	return nil
+}
