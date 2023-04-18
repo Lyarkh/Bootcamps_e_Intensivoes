c0349f26ca6797b5c46a4d6a0a5d9a2465055d17
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-04-18 08:39:26 -0300

    feat: criando structs Chat e ChatConfig

--- chatservice/internal/domain/entity/chat.go
+++ chatservice/internal/domain/entity/chat.go
@@ -1,1 +1,13 @@
 package entity
+
+type ChatConfig struct {
+	Model *Model
+}
+
+type Chat struct {
+	ID         string
+	UserID     string
+	Status     string
+	TokenUsage int
+	Config     *ChatConfig
+}
