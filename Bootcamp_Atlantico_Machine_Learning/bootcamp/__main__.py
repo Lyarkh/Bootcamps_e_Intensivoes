6bff634e141d6e82edeced363b6b1b3c0638648a
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-06 15:49:10 -0300

    feat: exemplo de busca das questões formatadas

--- Bootcamp_Atlantico_Machine_Learning/bootcamp/__main__.py
+++ Bootcamp_Atlantico_Machine_Learning/bootcamp/__main__.py
@@ -4,4 +4,18 @@ sys.path.insert(0, os.getcwd())
 
 from bootcamp.utils.leitor_de_questoes import LeitorDeQuestoes
 
-print(LeitorDeQuestoes(1).run())
+
+num_modulo = input('Digite o numero do modulo: ')
+
+try:
+    num_modulo = int(num_modulo)
+
+    flag_existe, questoes = LeitorDeQuestoes(num_modulo).run()
+
+    if flag_existe:
+        print(questoes)
+    else:
+        print('não existe questoes para este modulo solicitado')
+
+except ValueError:
+    print('digite um numero de modulo válido')
