18687b3cd94181bd9f2bd8c2f37793eb0cf2c254
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-10-26 20:11:03 -0300

    refactor: alterando parametros

--- Imersao_Full_Stack_e_Full_Cycle/transcoding/cmd/videoconverter/main.go
+++ Imersao_Full_Stack_e_Full_Cycle/transcoding/cmd/videoconverter/main.go
@@ -4,5 +4,5 @@ import "imersaofc/transcoding/internal/converter"
 
 func main() {
 	vc := converter.NewVideoConverter()
-	vc.Handle([]byte(`{"video_id": "1", "path": "mediatest/media/uploads/1"}`))
+	vc.Handle([]byte(`{"video_id": 1, "path": "/media/uploads/1"}`))
 }
