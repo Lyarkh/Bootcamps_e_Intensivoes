374d048ba8513f8338a653ee4bcf381b5313afcf
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-10-26 19:42:11 -0300

    feat: criando metodos para videoConverter

--- Imersao_Full_Stack_e_Full_Cycle/transcoding/internal/converter/task.go
+++ Imersao_Full_Stack_e_Full_Cycle/transcoding/internal/converter/task.go
@@ -2,7 +2,14 @@ package converter
 
 import (
 	"encoding/json"
+	"fmt"
 	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
 	"time"
 )
 
@@ -18,8 +25,42 @@ func (vc *VideoConverter) Handle(msg []byte) {
 	var task VideoTask
 	err := json.Unmarshal(msg, &task)
 	if err != nil {
-		panic(err)
+		vc.logErrror(task, "failed to unmarshal task", err)
 	}
+}
+
+func (vc *VideoConverter) processVideo(task *VideoTask) error {
+	mergedFile := filepath.Join(task.Path, "merged.mp4")
+	mpegDashPath := filepath.Join(task.Path, "mpeg-dash")
+
+	slog.Info("Merging chunks", slog.String("path", task.Path))
+	err := vc.mergeChunks(task.Path, mergedFile)
+	if err != nil {
+		vc.logErrror(*task, "failed to merge chunks", err)
+		return fmt.Errorf("failed to merge chunks: %v", err)
+	}
+
+	slog.Info("Creating mpeg-dash dir", slog.String("path", task.Path))
+	err = os.MkdirAll(mpegDashPath, os.ModePerm)
+	if err != nil {
+		vc.logErrror(*task, "failed to create mpeg-dash directory", err)
+		return fmt.Errorf("failed to create mpeg-dash directory: %v", err)
+	}
+
+	slog.Info("Convertin video to mpeg-dash", slog.String("path", task.Path))
+	ffmpegCmd := exec.Command(
+		"ffmpeg", "-i", mergedFile,
+		"-f", "dash",
+		filepath.Join(mpegDashPath, "output.mpd"),
+
+	)
+
+	output, err := ffmpegCmd.CombinedOutput()
+	if err != nil {
+		vc.logErrror(*task, "failed to convert video to mpeg-dash, output:" + string(output), err)
+		return err
+	}
+	return nil
 }
 
 func (vc *VideoConverter) logErrror(task VideoTask, message string, err error) {
@@ -32,4 +73,47 @@ func (vc *VideoConverter) logErrror(task VideoTask, message string, err error) {
 
 	serializedError, _ := json.Marshal(errorData)
 	slog.Error("Processing error", slog.String("error_details", string(serializedError)))
+	// todo: register error on database
+}
+
+func (vc *VideoConverter) extractNumber(fileName string) int {
+	re := regexp.MustCompile(`\d+`)
+	numStr := re.FindString(filepath.Base(fileName)) // String
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return -1
+	}
+	return num
+}
+
+func (vc *VideoConverter) mergeChunks(inputDir, outputFile string) error {
+	chunks, err := filepath.Glob(filepath.Join(inputDir, "*.chunk"))
+	if err != nil {
+		return fmt.Errorf("failed to find chhunks %v", err)
+	}
+
+	sort.Slice(chunks, func(i, j int) bool {
+		return vc.extractNumber(chunks[i]) < vc.extractNumber(chunks[j])
+	})
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file %v", err)
+	}
+	defer output.Close()
+
+	for _, chunk := range chunks {
+		input, err := os.Open(chunk)
+		if err != nil {
+			return fmt.Errorf("failed to open chunk: %v", err)
+		}
+
+		_, err = output.ReadFrom(input)
+		if err != nil {
+			return fmt.Errorf("failed to write chunk %s to merge file: %v", chunk, err)
+		}
+		input.Close()
+	}
+
+	return nil
 }
