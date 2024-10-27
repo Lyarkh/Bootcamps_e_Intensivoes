e70f6b5e789f7658130a1534aeece4a7a96e4b95
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-10-27 20:11:34 -0300

    feat: adicionando idempontência para verificar se ja foi processado e também registrar caso ocorra erro e implementando comexão database

--- Imersao_Full_Stack_e_Full_Cycle/transcoding/cmd/videoconverter/main.go
+++ Imersao_Full_Stack_e_Full_Cycle/transcoding/cmd/videoconverter/main.go
@@ -1,8 +1,50 @@
 package main
 
-import "imersaofc/transcoding/internal/converter"
+import (
+	"database/sql"
+	"fmt"
+	"imersaofc/transcoding/internal/converter"
+	"log/slog"
+	"os"
+)
+
+func connectPostgress() (*sql.DB, error) {
+	user := getEnvOrDefault("POSTGRES_USER", "user")
+	password := getEnvOrDefault("POSTGRES_PASSWORD", "password")
+	dbname := getEnvOrDefault("POSTGRES_DB", "converter")
+	host := getEnvOrDefault("POSTGRES_HOST", "postgres")
+	sslmode := getEnvOrDefault("POSTGRES_SSLMODE", "disable")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=%s", user, password, dbname, host, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		slog.Error("Error connecting to database", slog.String("connStr", connStr))
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		slog.Error("Error pinging database", slog.String("connStr", connStr))
+		return nil, err
+	}
+
+	slog.Info("Connected to database successfully")
+	return db, nil
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultValue
+}
 
 func main() {
-	vc := converter.NewVideoConverter()
+	db, err := connectPostgress()
+	if err != nil {
+		panic(err)
+	}
+	vc := converter.NewVideoConverter(db)
 	vc.Handle([]byte(`{"video_id": 1, "path": "/media/uploads/1"}`))
 }
--- Imersao_Full_Stack_e_Full_Cycle/transcoding/internal/converter/task.go
+++ Imersao_Full_Stack_e_Full_Cycle/transcoding/internal/converter/task.go
@@ -1,6 +1,7 @@
 package converter
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -13,10 +14,14 @@ import (
 	"time"
 )
 
-type VideoConverter struct{}
+type VideoConverter struct {
+	db *sql.DB
+}
 
-func NewVideoConverter() *VideoConverter {
-	return &VideoConverter{}
+func NewVideoConverter(db *sql.DB) *VideoConverter {
+	return &VideoConverter{
+		db: db
+	}
 }
 
 // {"video_id": "1", "path": "media/uploads/1"}
@@ -33,11 +38,25 @@ func (vc *VideoConverter) Handle(msg []byte) {
 		return
 	}
 
+	if IsProcessed(vc.db, task.VideoID) {
+		slog.Warn("Video already processed", slog.Int("video_id", task.VideoID))
+		return
+	}
+
 	err = vc.processVideo(&task)
 	if err != nil {
 		vc.logErrror(task, "failed to process video", err)
 		return
 	}
+
+	err = MarkProcessed(vc.db, task.VideoID)
+	if err != nil {
+		vc.logErrror(task, "failed to mark video as processed", err)
+		return
+	}
+
+	slog.Info("Video marked as processd", slog.Int("video_id", task.VideoID))
+	
 }
 
 func (vc *VideoConverter) processVideo(task *VideoTask) error {
@@ -63,19 +82,18 @@ func (vc *VideoConverter) processVideo(task *VideoTask) error {
 		"ffmpeg", "-i", mergedFile,
 		"-f", "dash",
 		filepath.Join(mpegDashPath, "output.mpd"),
-
 	)
 
 	output, err := ffmpegCmd.CombinedOutput()
 	if err != nil {
-		vc.logErrror(*task, "failed to convert video to mpeg-dash, output:" + string(output), err)
+		vc.logErrror(*task, "failed to convert video to mpeg-dash, output:"+string(output), err)
 		return err
 	}
 
 	slog.Info("Video converted to mpeg-dash", slog.String("path", mpegDashPath))
 
 	slog.Info("Removing merged file", slog.String("path", mergedFile))
-	err =  os.Remove(mergedFile)
+	err = os.Remove(mergedFile)
 	if err != nil {
 		vc.logErrror(*task, "failed to remove merged file", err)
 		return err
@@ -94,7 +112,8 @@ func (vc *VideoConverter) logErrror(task VideoTask, message string, err error) {
 
 	serializedError, _ := json.Marshal(errorData)
 	slog.Error("Processing error", slog.String("error_details", string(serializedError)))
-	// todo: register error on database
+
+	RegisterError(vc.db, errorData, err)
 }
 
 func (vc *VideoConverter) extractNumber(fileName string) int {
